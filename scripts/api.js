905afc83b8f2eb1be94aef60517361659d023d3c
rubenbot77 <rubenBot77@hotmail.com>
2023-11-08 15:06:17 -0500

    delete img directory, trending movies working

--- scripts/api.js
+++ scripts/api.js
@@ -2,7 +2,23 @@ async function getTrendingMoviesPreview() {
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json();
     const movies = data.results;
+    const limit = movies.slice(0,9)
     console.log(movies);
+
+    limit.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('.swiper-wrapper') 
+
+        const movieContainer = document.createElement('div')
+        movieContainer.classList.add('swiper-slide')
+
+        const movieImg = document.createElement('img')
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300'+ movie.poster_path,)
+        
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer)
+    })
+
 }
 
 getTrendingMoviesPreview()
