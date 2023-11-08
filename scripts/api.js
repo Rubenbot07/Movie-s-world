d775fff3fbaa64b115ab31255cee2db948a7e209
rubenbot77 <rubenBot77@hotmail.com>
2023-11-08 14:25:15 -0500

    trending movies from tmdb

---
+++ scripts/api.js
@@ -0,0 +1,8 @@
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+    const movies = data.results;
+    console.log(movies);
+}
+
+getTrendingMoviesPreview()
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,5 @@
+import './api.js';
 import './nav.js';
 import './slider.js';
+
+
